9907af24f6c412333bb566fdf5dfdf4bc16e95a2
Jonah Schneider <Redflag11104@gmail.com>
2026-05-28 17:37:54 -0400

    Added SQLite schema and starter data

--- database/schema.sql
+++ database/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Employees (
     EmployeeID INTEGER PRIMARY KEY,
-    Name TEXT
+    FullName TEXT
 );
 
 CREATE TABLE Equipment (
@@ -16,3 +16,22 @@ CREATE TABLE Transactions (
     CheckoutTime TEXT,
     ReturnTime TEXT
 );
+
+INSERT INTO Employees (EmployeeID, FullName)
+VALUES 
+(1001, 'Jonah Schneider'),
+(1002, 'Michael Mottie'),
+(1003, 'Adriel Spencer'),
+(1004, 'Clinton Hockenberry'),
+(1005, 'Anthony Dumsar');
+
+
+INSERT INTO Equipment (EquipmentID, EquipmentName, Status)
+VALUES
+(2001, 'Drill', 'Available'),
+(2002, 'Hammer', 'Available'),
+(2003, 'Wrench', 'Available'),
+(2004, 'Saw', 'Available'),
+(2005, 'Ladder', 'Available'),
+(2006, 'Toolbox', 'Available'),
+(2007, 'Safety Helmet', 'Available');
